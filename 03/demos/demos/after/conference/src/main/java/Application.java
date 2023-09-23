e1abb77d206abdbc295fd93d48b242325406534e
Eagle Dai <dynuaa@gmail.com>
2023-09-23 23:38:52 +0800

    03 demo after conference update

--- 03/demos/demos/after/conference/src/main/java/Application.java
+++ 03/demos/demos/after/conference/src/main/java/Application.java
@@ -3,7 +3,7 @@ import com.pluralsight.service.SpeakerServiceImpl;
 
 public class Application {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         SpeakerService service = new SpeakerServiceImpl();
 
         System.out.println(service.findAll().get(0).getFirstName());
